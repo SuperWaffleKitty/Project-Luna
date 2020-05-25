1f39a2402b29f0836cd4a30d61e113fae306b2cd
SuperWaffleKitty <solomonrthatcher@gmail.com>
2020-05-25 17:31:21 +0000

    Added ability to post pictures

--- Luna.py
+++ Luna.py
@@ -4,7 +4,7 @@ import config
 from discord.ext import commands
 
 
-client = commands.Bot(command_prefix = '! ')
+client = commands.Bot(command_prefix = '!')
 client.remove_command('help')
 
 
--- cogs/Fuck.py
+++ cogs/Fuck.py
@@ -13,9 +13,16 @@ class Fuck(commands.Cog):
         print('Providing "Content"')
 
 
+    #@commands.command()
+    #async def script(self, ctx):
+    #    await ctx.send('```Currently, the sonic script is unavailable```')
+    #    await ctx.send('```THIS IS NOT EBIC```')
+
+
     @commands.command()
     async def fuck(self, ctx):
-        await ctx.send('https://e621.net/posts/' + random.randint(100,2270250)
+        link = random.randint(100,2270250)
+        await ctx.send('https://e621.net/posts/' + str(link))
 
 
 def setup(client):
