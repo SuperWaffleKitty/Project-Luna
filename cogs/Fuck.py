a7cdfb0f57ed8bf48444bc950f0ef3df14da4f0b
JezzaR <54776821+JezzaR-The-Proto@users.noreply.github.com>
2020-06-15 22:59:20 +0100

    Updated so it uses to E6 API

--- cogs/Fuck.py
+++ cogs/Fuck.py
@@ -1,27 +1,19 @@
 import discord
 import random
+import requests
 from discord.ext import commands
 
 class Fuck(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-
-
+    
     @commands.Cog.listener()
     async def on_ready(self):
         print('Providing "Content"')
 
-
-    #@commands.command()
-    #async def script(self, ctx):
-    #    await ctx.send('```Currently, the sonic script is unavailable```')
-    #    await ctx.send('```THIS IS NOT EBIC```')
-
-
-
-
     @commands.command()
+    @commands.cooldown(1,3,commands.BucketType.user)
     async def random(self, ctx):
 
         if not isinstance(ctx.channel, discord.DMChannel):
@@ -29,9 +21,16 @@ class Fuck(commands.Cog):
                     if not ctx.channel.is_nsfw():
                         await ctx.send("Cannot be used in non-NSFW channels!")
                         return
+                        
+        headers = {"User-Agent":"Project-Luna/1.0 (API Usage by jezzar on E621)"}
         link = random.randint(100,2270250)
-        await ctx.send('https://e621.net/posts/' + str(link))
-    #https://e621.net/posts?page=2&tags=anthro
+        Req = requests.get(f"https://e621.net/posts.json?tags=id:{link}&rating:explicit", headers=headers)
+        ReqJson = Req.json()
+        if Req.status_code != 200:
+            await ctx.send(f"Couldn't contact e621. Error code: {Req.status_code}.\nJson: {ReqJson}")
+            return
+        Post = ReqJson["posts"][0]["file"]["url"]
+        await ctx.send(f"Here is your random yiff: {Post}")
 
     @commands.command()
     async def me(self, ctx):
@@ -45,9 +44,5 @@ class Fuck(commands.Cog):
     async def yourself(self, ctx):
         await ctx.send("Why don't you do it for me pussy.")
 
-
 def setup(client):
     client.add_cog(Fuck(client))
-#end
-#text
-#HEROKU TEST
