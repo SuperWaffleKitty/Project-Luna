c183f280a6bfc7ae0c878b3c63241e4ce7ff6e88
superwafflekitty <solomonrthatcher@gmail.com>
2020-07-10 12:00:12 -0400

    Updated starting bash file to detect python version

--- start.sh
+++ start.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 #!/usr/bin/env python3
-python3 Luna.py
+FILE=Luna.py
+ver=$(python -c"import sys; print(sys.version_info.major)")
+
+echo 'attempting to start executable...'
+
+if [ -f "$FILE" ]; then
+  echo "File is present..."
+
+  if [ $ver -eq 3 ]; then
+      echo "Python is up to date..."
+      echo "Starting Executable..."
+      python3 Luna.py
+
+  else
+      echo "Please upgrade to Python3"
+  fi
+
+else
+    echo "$FILE does not exist, check to make sure you have the latest version."
+fi
